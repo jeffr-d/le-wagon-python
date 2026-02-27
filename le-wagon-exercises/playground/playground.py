006239efb17e41f9fdee0939a2f81f104cc3162d
jeffr-d <yz9p8pz6zh@privaterelay.appleid.com>
2026-02-27 15:15:09 +0100

    Update playground.py

--- le-wagon-exercises/playground/playground.py
+++ le-wagon-exercises/playground/playground.py
@@ -1,1 +1,27 @@
-# TODO: it's a playground, let's write some code (no unit tests to run here)import mathradius = 5perimeter = round(2 * math.pi * radius, 1)area = round(math.pi * radius ** 2, 1)circle_data = f"The radius is set to {radius}\nPerimeter of the circle is {perimeter}\nArea of the disk is {area}"print(circle_data)from math import pi# defining the functiondef circle_math(radius):    perimeter = 2 * pi * radius    area = pi * radius * radius    return [ perimeter, area ] # perimeter and area are called **local variables* # 2. calling the function# for radius 5values = circle_math(5)print(f"Radius=5 => Perimeter={round(values[0], 1)}, Area={round(values[1], 1)}")# for radius 6 - reusing "values"values = circle_math(6)print(f"Radius=6 => Perimeter={round(values[0], 1)}, Area={round(values[1], 1)}")
+import math
+
+radius = 5
+perimeter = round(2 * math.pi * radius, 1)
+area = round(math.pi * radius ** 2, 1)
+
+circle_data = f"The radius is set to {radius}\nPerimeter of the circle is {perimeter}\nArea of the disk is {area}"
+
+print(circle_data)
+
+from math import pi
+# defining the function
+def circle_math(radius):
+    perimeter = 2 * pi * radius
+    area = pi * radius * radius
+    return [ perimeter, area ] # perimeter and area are called **local variables* 
+
+# 2. calling the function
+# for radius 5
+values = circle_math(5)
+print(f"Radius=5 => Perimeter={round(values[0], 1)}, Area={round(values[1], 1)}")
+
+# for radius 6 - reusing "values"
+values = circle_math(6)
+print(f"Radius=6 => Perimeter={round(values[0], 1)}, Area={round(values[1], 1)}")
+
+
